2712b1fef2b590bf2b6318c4afa97f6389c236a5
Koade <rmazwiers@gmail.com>
2016-09-10 17:01:45 -0400

    Testing JS

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ $(".hamburger-menu").toggleClass("invis");
 $(document).ready(function() {
 $(".nav-item").hover(function() {
 $(this).css("background-color","#cc1817");
-$(this p).css("color","#ffffff");
+$(this).css("color","#ffffff");
 }, function() {
 $(this).css("background-color","#ffffff");
-$(this p).css("color","#f18700");
+$(this).css("color","#f18700");
 });
 });
 
@@ -20,4 +20,12 @@ $(".ddmenu, .dditem").hover(function() {
 $(".dditem").css("display","block");
 }, function() {
 $(".dditem").css("display","none");
+});
+
+$(document).ready(function() {
+$(".container").hover(function() {
+$(this).css("border","5px solid #36CFC8");
+}, function() {
+$(this).css("border","5px solid #b8b8b8");
+});
 });
