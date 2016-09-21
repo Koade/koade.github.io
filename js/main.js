d96454851fa1084a557587bd022cb15e141de0b2
Koade <rmazwiers@gmail.com>
2016-09-21 16:02:49 -0400

    fixes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(".navibar").toggleClass("fullwidth");
 $(document).ready(function() {
 $(".nav-item").hover(function() {
 $(this).css("background-color","#F18601");
-$(this).css("color","#ffffff");
+$(this).css("color","#ffffff !important");
 }, function() {
 $(this).css("background-color","#ffffff");
 $(this).css("color","#f18601");
@@ -19,7 +19,7 @@ $(this).css("color","#f18601");
 $(document).ready(function() {
 $(".ddmenu, .dditem").hover(function() {
 $(".dditem").css("display","block");
-$(".content").css("margin-top","50vh")
+$(".content").css("margin-top","40vh")
 }, function() {
 $(".dditem").css("display","none");
 $(".content").css("margin-top","25vh")
